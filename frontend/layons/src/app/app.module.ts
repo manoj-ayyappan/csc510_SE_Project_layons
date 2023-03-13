9d437348ae609fa9165f3e3f6b96da36c66c6a13
kgopala3 <kgopala3@ncsu.edu>
2023-03-12 22:32:03 -0400

    Add Profile Components

--- frontend/layons/src/app/app.module.ts
+++ frontend/layons/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
