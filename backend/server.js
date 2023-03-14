fcfbec18ec012fd7f0b5972a0e9dc3b308060571
mayyapp <mayyapp@ncsu.edu>
2023-03-14 16:55:20 -0400

    Added new get request

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app  = express();
-const job_id = 1;
+let job_id = 0;
+let user_id = 0;
 
 const bodyParser = require('body-parser');
 
@@ -11,23 +12,37 @@ app.listen(8082);
 console.log("App running! yay");
 
 app.get('/jobs', (req, res) => {
-    const {title} = req.params;
+    let title = req.params;
     res.send(jobs_array)
 })
 
-app.post('/send-jobs', (req, res) => {
-    const job = req.body;
+app.post('/jobs', (req, res) => {
+    let job = req.body;
     job_id += 1;
+    job.jobId = job_id
     jobs_array.push(job);
-    res.send(jobs_array)
+    res.send(job)
+})
+
+app.get('/all-jobs', (req, res) => {
+    for (let i = 0, len = jobs_array.length, text=""; i < len; i++) {
+        text += jobs_array[i] + "<br>";
+      }
+    res.send(text)
 })
 
 app.post('/send-user', (req, res) => {
-    const user = req.body;
-    users_array.push(user);
+    let user = req.body;
+    user_id += 1;
+    users_array.push(user_id, user);
     res.send(users_array)
   })
 
-const jobs_array = [{name: "Manoj", age: "22"}];
+app.get('/jobs/:jobid', (req, res) => {
+    let id_req = req.body;
+    res.send(jobs_array.get(id_req))
+})
+
+const jobs_array = [{jobid: "3", title: "SWE2"}];
 const users_array = [];
 
