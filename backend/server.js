33522c87864590b59b9c295038b950e2ebc712d0
mayyapp <mayyapp@ncsu.edu>
2023-03-16 20:35:32 -0400

    Corrected string to array

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app  = express();
 let job_id = 0;
 let user_id = 0;
-let text = "";
+let text = [];
 
 const bodyParser = require('body-parser');
 
@@ -22,34 +22,44 @@ app.post('/jobs', (req, res) => {
     let job = req.body;
     job_id += 1;
     job.jobId = job_id
-    jobs_array.set("title", job);
+    jobs_array.set(job_id, job);
     res.send(job)
 })
 
 // get all jobs
 app.post('/all-jobs', (req, res) => {
+    let jobs_arr = []
     for (let i = 0, len = jobs_array.length; i < len; i++) {
-        text += jobs_array[i] + "<br>";
+        jobs_arr.push(jobs_array[i]);
       }
-    res.send(text)
+    res.send(jobs_arr)
+})
+
+// get all users
+app.post('/all-users', (req, res) => {
+    let users_arr = []
+    for (let i = 0, len = users_array.length; i < len; i++) {
+        users_arr.push(users_array[i]);
+      }
+    res.send(users_arr)
 })
 
 // send user info
 app.post('/send-user', (req, res) => {
     let user = req.body;
     user_id += 1;
-    users_array.set("user_id", user_id);
-    users_array.set("user", user);
+    user.user_id = user_id;
+    // users_array.set("user_id", user_id);
+    users_array.set(user_id, user);
     res.send(user)
   })
 
 
 app.post('/jobs/:jobid', (req, res) => {
     
-    let id_req = req.body;
+    let id_req = req.body.job_id.toString();
     // console.log(typeof(id_req));
-    // test data = 3
-    res.send(jobs_array.get("3"))
+    res.send(jobs_array.get(id_req))
 })
 
 var jobs_array = new Map([["3", "SWE2"], ["4", "Test Engineer"]]);
