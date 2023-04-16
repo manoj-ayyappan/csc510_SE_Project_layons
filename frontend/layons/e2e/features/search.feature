e93832bcc8e1d6635e9d4e4ac072d6db474e7b9b
snaray24 <snaray24@ncsu.edu>
2023-04-16 11:09:20 -0400

    search feature half done untested

--- frontend/layons/e2e/features/search.feature
+++ frontend/layons/e2e/features/search.feature
@@ -9,10 +9,10 @@ Feature: Create Jobs
         When I enter a job’s name as "SDE 1"
         When I click the button called Search
         Then I am given a list of jobs that match "SDE 1"
-        Then all the job listings have a button “Apply”
-        When I click the button “Apply” of "SDE 1" from that list
+        Then all the job listings have a button "Apply"
+        When I click the button "Apply" of "SDE 1" from that list
         Then I am redirected to the job page of that particular job
         Then I can see job details such as job description, employer’s email, employer’s name, pay range, work location
-        When I click on “Apply” button to apply for that job
+        When I click on "Apply" button to apply for that job
         Then A message is displayed “Applied Successfully” 
         Then I stay on the same page at the end
--- frontend/layons/e2e/steps/search.step.ts
+++ frontend/layons/e2e/steps/search.step.ts
@@ -6,6 +6,7 @@ let expect = chai.expect;
 let jobLinks = [],
   jobsList = [];
 let visitedJob = '';
+let jobDetails = {};
 
 function searchsubsequence(query: string, jobName: string) {
   if (!query) return true;
@@ -70,7 +71,7 @@ Then('all the job listings have a button “Apply”', async () => {
 When(
   'I click the button “Apply” of {string} from that list',
   async (jobName) => {
-    console.log(`Here ${jobName}`);
+    /* console.log(`Here ${jobName}`); */
     let jobButton: any = null;
     await element(by.css('ul'))
       .all(by.css('li .job-name'))
@@ -85,6 +86,8 @@ When(
       });
     console.log('Here', visitedJob);
     console.log('Here', jobButton);
+    let jobid = visitedJob.substring(visitedJob.lastIndexOf('/') + 1);
+    jobDetails = await fetch(`http://localhost:3000/jobs/${jobid}`);
     expect(jobButton).to.not.be.null;
     if (jobButton != null) {
       console.log('Here', jobButton, visitedJob);
@@ -99,15 +102,43 @@ Then('I am redirected to the job page of that particular job', async () => {
 
 Then(
   'I can see job details such as job description, employer’s email, employer’s name, pay range, work location',
-  () => {}
+  async () => {
+    let input = await $('span.title');
+    let title = await input.getText();
+    input = await $('span.description');
+    let desc = await input.getText();
+    input = await $('span.payrangemin');
+    let minpay = await input.getText();
+    input = await $('span.payrangemax');
+    let maxpay = await input.getText();
+    input = await $('span.email');
+    let email = await input.getText();
+    input = await $('span.location');
+    let location = await input.getText();
+    input = await $('span.employerName');
+    let employerName = await input.getText();
+    expect(title).to.eventually.equal(jobDetails.title);
+    expect(description).to.eventually.equal(jobDetails.description);
+    expect(payrangemax).to.eventually.equal(jobDetails.payrangemax);
+    expect(payrangemin).to.eventually.equal(jobDetails.payrangemin);
+    expect(email).to.eventually.equal(jobDetails.email);
+    expect(location).to.eventually.equal(jobDetails.location);
+    expect(employerName).to.eventually.equal(jobDetails.employerName);
+  }
 );
 
-When('I click on “Apply” button to apply for that job', () => {});
+When('I click on “Apply” button to apply for that job', () => {
+  await element(by.buttonText('Apply')).click();
+});
 
-Then('A message is displayed “Applied Successfully”', () => {});
+Then('A message is displayed “Applied Successfully”', () => {
+  let ele = await element(by.buttonText('Applied'));
+  let exists = ele.isPresent();
+  expect(exists).to.be.true;
+});
 
 Then('I stay on the same page at the end', async () => {
   let url = await browser.getCurrentUrl();
   expect(await browser.getTitle()).to.eventually.equal('Layons');
-  /* expect(url).to.equal(`http://localhost:4200/jobs/${jobid}`) */
+  expect(url).to.equal(visitedJob);
 });
